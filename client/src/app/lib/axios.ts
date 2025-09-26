cab42b960ae334f3435b396a50a5712df36d1b21
mosarof66 <devmosarof.123@gmail.com>
2025-09-26 18:52:06 +0600

    backend url added

--- client/src/app/lib/axios.ts
+++ client/src/app/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: `${process.env.BACKEND_URL}/api/v1`,
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
